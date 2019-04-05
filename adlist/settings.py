b5d52f52abf179457e97f2456f605150eec609b5
Hannah Rauh <hrauh@umich.edu>
2019-04-05 10:54:31 -0400

    added comments

--- adlist/settings.py
+++ adlist/settings.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Used for a default title
-APP_NAME = 'Griff_Adlist'   # Add
+APP_NAME = 'HannahsList'   # Add
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
